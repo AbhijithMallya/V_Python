a58dea2ce5ca7ce1e1006988e4cda716747310a6
ABHIJITH MALLYA <abhijithmallya@gmail.com>
2022-11-03 23:22:21 +0530

    Rock Paper Scissors

--- Rock_paper.py
+++ Rock_paper.py
@@ -3,9 +3,28 @@
         USN :: 4SF20CI002
 """
 import random
+import os
+human_choice=0
+user_score = computer_score = 0
 
-print("1 --> Rock")
-print("2 --> Paper")
-print("3 --> Scissor")
+while(human_choice != 4 ):
+    os.system('cls')
+    print("HUMAN :: {}\tVS\tCOMPUTER:: {}\n".format(user_score,computer_score))    
+    print("1 --> Rock")
+    print("2 --> Paper")
+    print("3 --> Scissor")
+    print("4 --> Exit")
 
-choice = int(input("Enter Choice :: "))
+    human_choice = int(input("Enter Choice :: "))
+    outcomes = { 1 : "Rock" , 2 : "Paper" , 3 : "Scissor"}
+    list_outcomes = list(outcomes.items())
+    key , value  = random.choice(list_outcomes)
+    print("Computer Choice :: " + value)
+
+    if human_choice == key :
+        print("\nTie!")
+    elif (human_choice == 1 and key == 3) or (human_choice == 2 and key == 1) or (human_choice == 3 and key == 2):
+        user_score = user_score + 1
+    else :
+        computer_score = computer_score + 1
+    input()
