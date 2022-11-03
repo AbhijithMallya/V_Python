e4f5aa3b3d1d0a4cb0d77541c2b747e0150c36bd
ABHIJITH MALLYA <abhijithmallya@gmail.com>
2022-11-03 22:53:10 +0530

    added choice

--- Rock_paper.py
+++ Rock_paper.py
@@ -2,4 +2,12 @@
         Name :: Abhijith Mallya
         USN :: 4SF20CI002
 """
-import rando
+import random
+
+print("1 --> Rock")
+print("2 --> Paper")
+print("3 --> Scissor")
+
+choice = int(input("Enter Choice :: "))
+
+
